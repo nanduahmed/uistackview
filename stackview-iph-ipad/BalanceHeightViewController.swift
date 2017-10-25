7a97f06af19eac4a27788137ff6b674629f4c03d
Ahmed <nandu.ahmed@sikkasoftware.com>
2017-10-25 11:43:37 -0700

    Solution for https://stackoverflow.com/questions/46888031/how-can-i-set-the-height-of-stackview-present-inside-another-stackview-whose-dis

---
+++ stackview-iph-ipad/BalanceHeightViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BalanceHeightViewController.swift
+//  stackview-iph-ipad
+//
+//  Created by Nandu Ahmed on 10/25/17.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+class BalanceHeightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
